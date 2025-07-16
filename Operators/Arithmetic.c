a4bacf9c23688fef0a6cba29ecf0338bcad0b57e
Saikat Talukder <saikat60mu@gmail.com>
2025-07-16 10:55:10 +0600

    Add files via upload

---
+++ Operators/Arithmetic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a = 105;
+   int b = 17;
+   printf("Addition : %d \n", a + b);
+   printf("subtraction: %d \n", a - b);
+   printf("multiplication: %d \n", a * b);
+   printf("division: %d \n", a / b);
+   printf("modulo: %d \n", a % b);
+
+   return 0;
+}
